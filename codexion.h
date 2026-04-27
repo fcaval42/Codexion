b5f1e3b8cc40dfa6bfa88c1951c997b62f4c8be6
Fleur Caval <fcaval@c1r1p4.42lehavre.fr>
2026-04-27 11:14:35 +0200

    fisrt commit

---
+++ codexion.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   codexion.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcaval <fcaval@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/27 10:48:04 by fcaval            #+#    #+#             */
+/*   Updated: 2026/04/27 11:11:36 by fcaval           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CODEXION_H
+# define CONEXION_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+
+
+typedef struct s_args
+{
+    
+} t_args
+
+
+#endif
